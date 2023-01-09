15551ae77ccf79b70d5f68f9f62a6b496d718e31
owais-ashraf <Owais.ashraf72@gmail.com>
2023-01-10 02:59:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let myNum = 10;
+
+var myNumb = 10;
+
+const myNumber = 10;
+
+myNum = 3 //This will change the value but if 
+// let myNum = 5; // this will give an error ("'myNum' has already been declared")
+
+myNumb = 3; // value will be change
+var myNumb = 5; // change the value without error
+
+// myNumber = 3; //const value cannot be change
+//  const myNumber = 5; // this will give an error ("'myNumber' has already been declared")
+
+console.log(myNum, myNumb, myNumber);
+
